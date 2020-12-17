1cb9b73849692c0d1cf9e6582cc11329e715cab2
Bassem-Yousry <Yousrybassem@gmail.com>
2020-12-17 12:55:15 +0200

    Update HLEDMAT_program.c

--- LED_Matrix_IR/src/HLEDMAT_program.c
+++ LED_Matrix_IR/src/HLEDMAT_program.c
@@ -16,24 +16,24 @@
 static u8 HLEDMAT_u8CurrentIdx;
 void HLEDMAT_voidInit(void)
 {
-	DIO_voidSetPinDirection(HLEDMAT_ROW_0,0b0010);
-	DIO_voidSetPinDirection(HLEDMAT_ROW_1,0b0010);
-	DIO_voidSetPinDirection(HLEDMAT_ROW_2,0b0010);
-	DIO_voidSetPinDirection(HLEDMAT_ROW_3,0b0010);
-	DIO_voidSetPinDirection(HLEDMAT_ROW_4,0b0010);
-	DIO_voidSetPinDirection(HLEDMAT_ROW_5,0b0010);
-	DIO_voidSetPinDirection(HLEDMAT_ROW_6,0b0010);
-	DIO_voidSetPinDirection(HLEDMAT_ROW_7,0b0010);
+	MGPIO_VidSetPinDirection(HLEDMAT_ROW_0,0b0010);
+	MGPIO_VidSetPinDirection(HLEDMAT_ROW_1,0b0010);
+	MGPIO_VidSetPinDirection(HLEDMAT_ROW_2,0b0010);
+	MGPIO_VidSetPinDirection(HLEDMAT_ROW_3,0b0010);
+	MGPIO_VidSetPinDirection(HLEDMAT_ROW_4,0b0010);
+	MGPIO_VidSetPinDirection(HLEDMAT_ROW_5,0b0010);
+	MGPIO_VidSetPinDirection(HLEDMAT_ROW_6,0b0010);
+	MGPIO_VidSetPinDirection(HLEDMAT_ROW_7,0b0010);
 	
 	
-	DIO_voidSetPinDirection(HLEDMAT_COL_0,0b0010);
-	DIO_voidSetPinDirection(HLEDMAT_COL_1,0b0010);
-	DIO_voidSetPinDirection(HLEDMAT_COL_2,0b0010);
-	DIO_voidSetPinDirection(HLEDMAT_COL_3,0b0010);
-	DIO_voidSetPinDirection(HLEDMAT_COL_4,0b0010);
-	DIO_voidSetPinDirection(HLEDMAT_COL_5,0b0010);
-	DIO_voidSetPinDirection(HLEDMAT_COL_6,0b0010);
-	DIO_voidSetPinDirection(HLEDMAT_COL_7,0b0010);
+	MGPIO_VidSetPinDirection(HLEDMAT_COL_0,0b0010);
+	MGPIO_VidSetPinDirection(HLEDMAT_COL_1,0b0010);
+	MGPIO_VidSetPinDirection(HLEDMAT_COL_2,0b0010);
+	MGPIO_VidSetPinDirection(HLEDMAT_COL_3,0b0010);
+	MGPIO_VidSetPinDirection(HLEDMAT_COL_4,0b0010);
+	MGPIO_VidSetPinDirection(HLEDMAT_COL_5,0b0010);
+	MGPIO_VidSetPinDirection(HLEDMAT_COL_6,0b0010);
+	MGPIO_VidSetPinDirection(HLEDMAT_COL_7,0b0010);
 }
 
 void HLEDMAT_voidDisplayFrame(u8 *Copy_u8Frame)
@@ -75,38 +75,38 @@ void HLEDMAT_voidDisplayShiftFrame(u8 *Copy_u8Frame,u8 Copy_u8Time,u8 Copy_u8Siz
 	Local_u8Bit[Local_u8Idx]=GET_BIT(Copy_u8Value,Local_u8Idx);
 	}
 	
-	DIO_voidSetPinValue(HLEDMAT_ROW_0,Local_u8Bit[0]);
-	DIO_voidSetPinValue(HLEDMAT_ROW_1,Local_u8Bit[1]);
-	DIO_voidSetPinValue(HLEDMAT_ROW_2,Local_u8Bit[2]);
-	DIO_voidSetPinValue(HLEDMAT_ROW_3,Local_u8Bit[3]);
-	DIO_voidSetPinValue(HLEDMAT_ROW_4,Local_u8Bit[4]);
-	DIO_voidSetPinValue(HLEDMAT_ROW_5,Local_u8Bit[5]);
-	DIO_voidSetPinValue(HLEDMAT_ROW_6,Local_u8Bit[6]);
-	DIO_voidSetPinValue(HLEDMAT_ROW_7,Local_u8Bit[7]);
+	MGPIO_VidSetPinValue(HLEDMAT_ROW_0,Local_u8Bit[0]);
+	MGPIO_VidSetPinValue(HLEDMAT_ROW_1,Local_u8Bit[1]);
+	MGPIO_VidSetPinValue(HLEDMAT_ROW_2,Local_u8Bit[2]);
+	MGPIO_VidSetPinValue(HLEDMAT_ROW_3,Local_u8Bit[3]);
+	MGPIO_VidSetPinValue(HLEDMAT_ROW_4,Local_u8Bit[4]);
+	MGPIO_VidSetPinValue(HLEDMAT_ROW_5,Local_u8Bit[5]);
+	MGPIO_VidSetPinValue(HLEDMAT_ROW_6,Local_u8Bit[6]);
+	MGPIO_VidSetPinValue(HLEDMAT_ROW_7,Local_u8Bit[7]);
 }
 
  void ActivateColumn(u8 Copy_u8Col)
 {
 	/* Disable All */
-	DIO_voidSetPinValue(HLEDMAT_COL_0,HIGH);
-	DIO_voidSetPinValue(HLEDMAT_COL_1,HIGH);
-	DIO_voidSetPinValue(HLEDMAT_COL_2,HIGH);
-	DIO_voidSetPinValue(HLEDMAT_COL_3,HIGH);
-	DIO_voidSetPinValue(HLEDMAT_COL_4,HIGH);
-	DIO_voidSetPinValue(HLEDMAT_COL_5,HIGH);
-	DIO_voidSetPinValue(HLEDMAT_COL_6,HIGH);
-	DIO_voidSetPinValue(HLEDMAT_COL_7,HIGH);
+	MGPIO_VidSetPinValue(HLEDMAT_COL_0,HIGH);
+	MGPIO_VidSetPinValue(HLEDMAT_COL_1,HIGH);
+	MGPIO_VidSetPinValue(HLEDMAT_COL_2,HIGH);
+	MGPIO_VidSetPinValue(HLEDMAT_COL_3,HIGH);
+	MGPIO_VidSetPinValue(HLEDMAT_COL_4,HIGH);
+	MGPIO_VidSetPinValue(HLEDMAT_COL_5,HIGH);
+	MGPIO_VidSetPinValue(HLEDMAT_COL_6,HIGH);
+	MGPIO_VidSetPinValue(HLEDMAT_COL_7,HIGH);
 	
 	/* Enable the desired pin only */
 	switch (Copy_u8Col)
 	{
-		case 0: DIO_voidSetPinValue(HLEDMAT_COL_0,LOW); break;
-		case 1: DIO_voidSetPinValue(HLEDMAT_COL_1,LOW); break;
-		case 2: DIO_voidSetPinValue(HLEDMAT_COL_2,LOW); break;
-		case 3: DIO_voidSetPinValue(HLEDMAT_COL_3,LOW); break;
-		case 4: DIO_voidSetPinValue(HLEDMAT_COL_4,LOW); break;
-		case 5: DIO_voidSetPinValue(HLEDMAT_COL_5,LOW); break;
-		case 6: DIO_voidSetPinValue(HLEDMAT_COL_6,LOW); break;
-		case 7: DIO_voidSetPinValue(HLEDMAT_COL_7,LOW); break;
+		case 0: MGPIO_VidSetPinValue(HLEDMAT_COL_0,LOW); break;
+		case 1: MGPIO_VidSetPinValue(HLEDMAT_COL_1,LOW); break;
+		case 2: MGPIO_VidSetPinValue(HLEDMAT_COL_2,LOW); break;
+		case 3: MGPIO_VidSetPinValue(HLEDMAT_COL_3,LOW); break;
+		case 4: MGPIO_VidSetPinValue(HLEDMAT_COL_4,LOW); break;
+		case 5: MGPIO_VidSetPinValue(HLEDMAT_COL_5,LOW); break;
+		case 6: MGPIO_VidSetPinValue(HLEDMAT_COL_6,LOW); break;
+		case 7: MGPIO_VidSetPinValue(HLEDMAT_COL_7,LOW); break;
 	}
 }
